b7870f4f790abe0b89224bc6e071f07e6a03efe9
Eggrollz <eggrollz@eggrollzs-mbp.lan>
2021-05-31 04:06:24 -0400

    Added sign in and sign up , homepage, profile page, notificatios, and messages

---
+++ components/Common/CommonInputs.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Form, Button, Message, TextArea, Divider } from "semantic-ui-react";
+
+function CommonInputs({
+  user: { about, linkedin, instagram, youtube, github },
+  handleChange,
+  showSocialLinks,
+  setShowSocialLinks
+}) {
+  return (
+    <>
+      <Form.Field
+        required
+        control={TextArea}
+        name="about"
+        value={about}
+        onChange={handleChange}
+        placeholder="About (Required)"
+      />
+
+      <Button
+        content="Add Social Links"
+        color="red"
+        icon="at"
+        type="button"
+        onClick={() => setShowSocialLinks(!showSocialLinks)}
+      />
+
+      {showSocialLinks && (
+        <>
+          <Divider />
+          <Form.Input
+            icon="linkedin"
+            iconPosition="left"
+            name="linkedin"
+            value={linkedin}
+            onChange={handleChange}
+          />
+
+          <Form.Input
+            icon="github"
+            iconPosition="left"
+            name="github"
+            value={github}
+            onChange={handleChange}
+          />
+
+          <Form.Input
+            icon="instagram"
+            iconPosition="left"
+            name="instagram"
+            value={instagram}
+            onChange={handleChange}
+          />
+
+          <Form.Input
+            icon="youtube"
+            iconPosition="left"
+            name="youtube"
+            value={youtube}
+            onChange={handleChange}
+          />
+
+          <Message
+            icon="attention"
+            info
+            size="small"
+            color="orange"
+            header="Your social network is your net worth!"
+          />
+        </>
+      )}
+    </>
+  );
+}
+
+export default CommonInputs;
