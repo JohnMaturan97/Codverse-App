3d4ff1f25e27cdb70537d2e97008ea45707bf2bd
JohnMaturan97 <borntocode1997@gmail.com>
2023-11-22 22:41:25 -0500

    PROD READY

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://codeverse-network-site.herokuapp.com";
+    : "https://codeverse-app-ce8650759529.herokuapp.com";
 
 module.exports = baseUrl;
